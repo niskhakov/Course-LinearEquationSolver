009b3dffbb04af047c91fdcb35811057790b888d
Nail Iskhakov <nail@iskhakov.me>
2019-01-21 22:03:34 +0300

    Module3 added

---
+++ Module3/TestProjects/src/TestCode.java
@@ -0,0 +1,9 @@
+public class TestCode {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = a; // 100 is copied to b
+
+        String lang = new String("java");
+        String java = lang;
+    }
+}
